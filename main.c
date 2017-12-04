10b18be3db06f264a82312656d814a53fd7da273
devjw2 <mcjeffrey168@gmail.com>
2017-12-03 23:34:04 -0500

    main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+int main(){
+  signal(SIGINT, sighandler);
+  while(1){
+    printf("\n");
+    print_current_dir();
+    char * input = get_input();
+    execute_input(input);
+    free(input);
+  }
+
+}
